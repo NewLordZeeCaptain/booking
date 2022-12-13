c34ed09b81b500fa8814ab9f3af0f69a64455fea
NewLordZeeCaptain <lordzeecaptain@ya.ru>
2022-12-13 14:46:28 +0300

    1

--- main.go
+++ main.go
@@ -2,14 +2,11 @@ package main
 
 import "log"
 
-type User struct {
-	FirstName string
-	LastName  string
-}
-
 func main() {
-
-	number := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	log.Println(number)
+	myNum := 100
+	isTrue := false
+	if myNum > 99 && !isTrue {
+		log.Println("myNum is > 99 and isTrue is set to true")
+	}
 
 }
